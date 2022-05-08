ede2bf102e3b9c464aacc8bbed9e9be88fe8aee7
Eilidh Quinn <eilidh.mcq@gmail.com>
2022-05-08 14:32:37 +0100

    EQ adding file structure

--- packedit/src/components/pages/Home/Home.js
+++ packedit/src/components/pages/Home/Home.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
+import "../../../styles/styles.css";
 
 const Home = () => {
   return (
