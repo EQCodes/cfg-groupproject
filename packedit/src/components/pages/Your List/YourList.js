1566d9e0a0c3a3439f1f8d1acc94f131573b0e34
Ada-Grace <21361216+Ada-Grace@users.noreply.github.com>
2022-05-08 18:02:58 +0100

    LA add item to items array plus category

--- packedit/src/components/pages/Your List/YourList.js
+++ packedit/src/components/pages/Your List/YourList.js
@@ -1,14 +1,36 @@
 import React from "react";
 import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
+import AddItem from './AddItem';
+//import categoryarray from './AddItem';
 
-const YourList = () => {
+
+
+
+const YourList = ({ categoryarray } ) => {
+  // Setting states
+  //This captures the user input
+  const [inputValue, setInputValue] = useState("");
+  //Used to hold the items
+  const [items, setItems] = useState([]);
+  //Used for categories
+  const [itemCategory, setItemCategory] = useState("");
+  //const [itemCategory, setItemCategory] = useState([categoryarray[1]]);
+
   return (
     <div class="app">
       <p>List</p>
       <nav>
         <Link to="/">Home</Link> | <Link to="/your-list">Your List</Link>
       </nav>
+      <AddItem 
+      inputValue={inputValue}
+      setInputValue={setInputValue}
+      items={items}
+      setItems={setItems}
+      itemCategory={itemCategory}
+      setItemCategory={setItemCategory}
+      />
     </div>
   );
 };
