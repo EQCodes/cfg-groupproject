7efa7949b896ab05088f23815e9b6dff4cbc1c1e
Eilidh Quinn <eilidh.mcq@gmail.com>
2022-05-10 12:39:12 +0100

    eq-go css tweaks

--- packedit/src/components/pages/Home/CreateYourList.js
+++ packedit/src/components/pages/Home/CreateYourList.js
@@ -4,7 +4,7 @@ import Button from 'react-bootstrap/Button';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Card from "react-bootstrap/Card";
-
+import '../../../styles/Card.css';
 
 function CreateYourList(props) {
     return (
