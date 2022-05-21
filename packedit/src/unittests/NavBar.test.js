f506096ba51e6ae608ed04713ba264a416d2d1cb
Eilidh Quinn <eilidh.mcq@gmail.com>
2022-05-21 19:56:22 +0100

    navbar unit test minor edit

--- packedit/src/unittests/NavBar.test.js
+++ packedit/src/unittests/NavBar.test.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import NavBar from '../../src/components/NavBar';
 
+
 it("NavBar renders without crashing", () => {
     const div = document.createElement("div");
     ReactDOM.render(<NavBar></NavBar>, div)
