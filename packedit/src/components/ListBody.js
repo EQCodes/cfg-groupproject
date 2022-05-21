c18f4ada7b7f59df949233ac172b8945e85ef8a2
BoGyeong Kim <kimbg9876@gmail.com>
2022-05-21 01:51:57 +0100

    Add Functionality: Delete Trip from Trips list(include Categories & items)

--- packedit/src/components/ListBody.js
+++ packedit/src/components/ListBody.js
@@ -54,7 +54,7 @@ function ListBody(props) {
     <>
         {myListCategories.map((category, i) => {
           return <div key={i}>
-            <h4>{category.CategoryName} ID: {category.id}</h4>
+            <h4>{category.CategoryName}</h4>
             <AddItem category={category.id} theTrip={props.theTrip} />
             <>{category.CategoryItems.map((item, j) => {
               return <div key={j}>
